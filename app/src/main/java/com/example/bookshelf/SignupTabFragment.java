b6e6de0a9400eff6b96f144f90c5d2a4b5d767b6
Negarnbkhti42 <norob35@gmail.com>
2023-05-27 14:34:58 +0430

    Create initial login fragment

--- app/src/main/java/com/example/bookshelf/SignupTabFragment.java
+++ app/src/main/java/com/example/bookshelf/SignupTabFragment.java
@@ -7,7 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-public class SignupTab extends Fragment {
+public class SignupTabFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
