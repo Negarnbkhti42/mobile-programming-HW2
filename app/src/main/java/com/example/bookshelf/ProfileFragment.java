b8d9e2b0dc68a5878a61a58d56a74daf7cd3dec6
Negarnbkhti42 <norob35@gmail.com>
2023-06-04 20:48:33 +0330

    Add password change functionality

--- app/src/main/java/com/example/bookshelf/ProfileFragment.java
+++ app/src/main/java/com/example/bookshelf/ProfileFragment.java
@@ -2,11 +2,16 @@ package com.example.bookshelf;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 
 public class ProfileFragment extends Fragment {
@@ -27,5 +32,34 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button editPasswordButton = (Button) view.findViewById(R.id.edit_password_button);
+        Button submitPasswordEdit = (Button) view.findViewById(R.id.submit_password_edit_button);
+
+        EditText newPasswordInput = (EditText) view.findViewById(R.id.new_password_input);
+
+        LinearLayout editPasswordField = (LinearLayout) view.findViewById(R.id.password_edit_field);
+
+        editPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editPasswordField.setVisibility(View.VISIBLE);
+                editPasswordButton.setVisibility(View.GONE);
+            }
+        });
+
+        submitPasswordEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newPasswordInput.setText("");
+                editPasswordField.setVisibility(View.GONE);
+                editPasswordButton.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
