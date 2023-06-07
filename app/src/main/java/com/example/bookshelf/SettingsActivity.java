67e0a3b16cd620fa46d9751610324bee05fd104b
Negarnbkhti42 <norob35@gmail.com>
2023-06-07 11:29:20 +0330

    Handle setting change

--- app/src/main/java/com/example/bookshelf/SettingsActivity.java
+++ app/src/main/java/com/example/bookshelf/SettingsActivity.java
@@ -1,10 +1,12 @@
 package com.example.bookshelf;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +17,19 @@ public class SettingsActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.settings_container, new SettingsFragment())
                 .commit();
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        System.out.println(sharedPreferences.getString("theme", "not found"));
+        if (s.equals("theme")) {
+            String theme = sharedPreferences.getString("theme", "light");
+
+            if (theme.equals("dark")) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            } else if (theme.equals("light")) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+        }
     }
 }
