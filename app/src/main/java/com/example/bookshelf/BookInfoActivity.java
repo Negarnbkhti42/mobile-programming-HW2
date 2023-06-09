350aecdfd9c47df76f2b87b661d7747aac39291f
Negarnbkhti42 <norob35@gmail.com>
2023-06-09 15:37:46 +0330

    Start purchase activity

--- app/src/main/java/com/example/bookshelf/BookInfoActivity.java
+++ app/src/main/java/com/example/bookshelf/BookInfoActivity.java
@@ -2,7 +2,10 @@ package com.example.bookshelf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BookInfoActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class BookInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_info);
+
+        Button buyBookButton = (Button) findViewById(R.id.buy_book_button);
+        buyBookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(BookInfoActivity.this, PurchaseActivity.class));
+            }
+        });
     }
 }
