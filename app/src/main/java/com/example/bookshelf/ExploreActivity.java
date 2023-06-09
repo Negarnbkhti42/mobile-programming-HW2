b9424d90527d652b8178e36fb2fc5de6b1ac2d63
Negarnbkhti42 <norob35@gmail.com>
2023-06-09 11:38:30 +0330

    Build recycler architecture

--- app/src/main/java/com/example/bookshelf/ExploreActivity.java
+++ app/src/main/java/com/example/bookshelf/ExploreActivity.java
@@ -1,14 +1,29 @@
 package com.example.bookshelf;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.bookshelf.adaptors.BookListAdaptor;
+
 public class ExploreActivity extends AppCompatActivity {
 
+    RecyclerView bookRecyclerView;
+    BookListAdaptor bookAdaptor;
+    RecyclerView.LayoutManager bookLayoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore);
+
+        bookRecyclerView = (RecyclerView) findViewById(R.id.book_recycler_view);
+
+        bookAdaptor = new BookListAdaptor(this, null);
+        bookRecyclerView.setAdapter(bookAdaptor);
+
+        bookLayoutManager = new LinearLayoutManager(this);
+        bookRecyclerView.setLayoutManager(bookLayoutManager);
     }
 }
--- app/src/main/java/com/example/bookshelf/viewholders/BookViewHolder.java
+++ app/src/main/java/com/example/bookshelf/viewholders/BookViewHolder.java
@@ -8,26 +8,31 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.bookshelf.R;
+import com.example.bookshelf.adaptors.BookListAdaptor;
 import com.google.android.material.imageview.ShapeableImageView;
 
 public class BookViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-    private TextView book_title;
-    private TextView book_author;
-    private TextView book_rating;
-    private TextView book_price;
-    private ShapeableImageView book_cover;
+    public TextView book_title;
+    public TextView book_author;
+    public TextView book_rating;
+    public TextView book_price;
+    public ShapeableImageView book_cover;
+
+    BookListAdaptor adaptor;
 
     private AdapterView.OnItemClickListener itemClickListener;
 
 
-    public BookViewHolder(@NonNull View itemView) {
+    public BookViewHolder(@NonNull View itemView, BookListAdaptor adaptor) {
         super(itemView);
-        book_title = (TextView) itemView.findViewById(R.id.book_title);
-        book_author = (TextView) itemView.findViewById(R.id.book_author);
-        book_rating = (TextView) itemView.findViewById(R.id.book_rating);
-        book_price = (TextView) itemView.findViewById(R.id.book_price);
-        book_cover = (ShapeableImageView) itemView.findViewById(R.id.book_cover);
+        this.book_title = (TextView) itemView.findViewById(R.id.book_title);
+        this.book_author = (TextView) itemView.findViewById(R.id.book_author);
+        this.book_rating = (TextView) itemView.findViewById(R.id.book_rating);
+        this.book_price = (TextView) itemView.findViewById(R.id.book_price);
+        this.book_cover = (ShapeableImageView) itemView.findViewById(R.id.book_cover);
+
+        this.adaptor = adaptor;
 
         itemView.setOnClickListener(this);
     }
@@ -35,6 +40,7 @@ public class BookViewHolder extends RecyclerView.ViewHolder implements View.OnCl
     public void setItemClickListener(AdapterView.OnItemClickListener itemClickListener) {
         this.itemClickListener = itemClickListener;
     }
+
 
     @Override
     public void onClick(View view) {
